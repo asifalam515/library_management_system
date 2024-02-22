14f60073e2869d6c98c5d2848c534a30fbf60a23
asifalam515 <asibulalam515@gmail.com>
2024-02-22 12:43:02 +0600

    super user created

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -61,6 +61,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        
+
     
 ]
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
